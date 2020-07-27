d6ae86ed9b0f34bf79b3e375916f5337deb20dd6
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-07-27 23:56:37 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="aiosyncthing",
-    version="0.1.0",
+    version="0.2.0",
     author="Gleb Sinyavskiy",
     author_email="zhulik.gleb@gmail.com",
     description="Asynchronous Python client for the Syncthing REST API",

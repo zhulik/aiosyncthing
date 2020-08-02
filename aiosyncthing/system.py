accb1ccd87936f6ae893da170089094f987a0e58
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2020-08-02 17:00:24 +0200

    Refactoring

--- aiosyncthing/system.py
+++ aiosyncthing/system.py
@@ -37,20 +37,26 @@ class System:
 
     async def pause(self, device_id=None):
         """Pause synchronization."""
-        params = {}
-        if device_id is not None:
-            params["device"] = device_id
         try:
-            await self._api.request("/rest/system/pause", method="POST", params=params)
+            await self._api.request(
+                "/rest/system/pause", method="POST", params=device_params(device_id)
+            )
         except NotFoundError as error:
             raise UnknownDeviceError from error
 
     async def resume(self, device_id=None):
         """Resume synchronization."""
-        params = {}
-        if device_id is not None:
-            params["device"] = device_id
         try:
-            await self._api.request("/rest/system/resume", method="POST", params=params)
+            await self._api.request(
+                "/rest/system/resume", method="POST", params=device_params(device_id)
+            )
         except NotFoundError as error:
             raise UnknownDeviceError from error
+
+
+def device_params(device_id):
+    """Build params hash from the device_id."""
+    params = {}
+    if device_id is not None:
+        params["device"] = device_id
+    return params
